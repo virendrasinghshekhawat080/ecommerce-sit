b23b9175bfe9439d776dcf5550df157cfdf197e3
Virendra Singh Shekhawat <virendrasinghshekhawat080@gmail.com>
2026-07-19 07:19:20 +0530

    updated base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Ecommerce-Website/"
-
-
-})
-
+git add.
